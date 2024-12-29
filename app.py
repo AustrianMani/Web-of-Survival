c0cc6eee7f8ae7df51810712064c062fcf51e2db
AustrianMani <87009797+AustrianMani@users.noreply.github.com>
2024-12-29 21:35:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import random, uuid
 import os
-from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Hello, Render!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Standardport 5000
